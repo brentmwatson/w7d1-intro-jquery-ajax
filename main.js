8020945c40617457eb7291906cb8121da99eced6
Brent Watson <brentmwatson@gmail.com>
2016-11-09 00:49:19 -0500

    Working and displaying Table

--- main.js
+++ main.js
@@ -2,19 +2,18 @@
 //jquery starts with $
 $.getJSON('https://fathomless-gorge-57039.herokuapp.com/api/all').then(function(data) {
     data.posts.forEach(function(recall) {
-        //pull avatar from website
-        if (recall.user.file == true) {
-            recall.user.file = "https://fathomless-gorge-57039.herokuapp.com"
-        }
-        else {
-            recall.user.file = "https://cdn.pixabay.com/photo/2016/03/31/14/47/avatar-1292817_960_720.png"
-        }
+        //Pull avatar from website
+        // if (recall.user.file == null) {
+        //     recall.user.file = "https://cdn.pixabay.com/photo/2016/03/31/14/47/avatar-1292817_960_720.png"
+        // }
+        // else {
+        //     recall.user.file = "https://fathomless-gorge-57039.herokuapp.com" + recall.user.file
+        // }
         //user avatar,
         //the body of the chirp,
         //the username
         //the time and date the chirp
         console.log(recall)
-
         $('.tweet').append('<tr><td>' + recall.user.file + '</td><td>' + recall.chirp + '</td><td>' + recall.user + '</td><td>' + recall.created_at + '</td>')
     })
 })

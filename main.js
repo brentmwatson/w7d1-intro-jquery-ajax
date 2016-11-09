b6cf1b601352266ac960c8314e8ba5105fc8086a
Brent Watson <brentmwatson@gmail.com>
2016-11-09 15:52:15 -0500

    small formatting changes to js file

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 //pull information from website
 //jquery starts with $
-$.getJSON('https://fathomless-gorge-57039.herokuapp.com/api/all').then(function(data) {
-    data.posts.forEach(function(recall) {
+$.getJSON('https://fathomless-gorge-57039.herokuapp.com/api/all')
+.then(function(data)
+    {
+    data.posts.forEach(function(recall)
+    {
         //Pull avatar from website
-        if (recall.user.file == null) {
+        if (recall.user.file == null)
+        {
             recall.user.file = "https://cdn.pixabay.com/photo/2016/03/31/14/47/avatar-1292817_960_720.png"
         }
-        else {
+        else
+        {
             recall.user.file = "https://fathomless-gorge-57039.herokuapp.com" + recall.user.file
         }
         //user avatar,
